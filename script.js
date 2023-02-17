e334872dca2f30c4d5fdc2b9cc797b4d73bf89a1
NahasNissar <51850372+NahasN@users.noreply.github.com>
2023-02-17 20:31:13 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const screen = document.getElementById('screen');
+
+function btnClick(value) {
+    screen.value += value;
+}
+
+
+function clearScreen() {
+    screen.value = "";
+}
+
+function findResult() {
+    const result = eval(screen.value)
+    screen.value = result;
+}
+
+function dot() {
+    let point = ".";
+    screen.value += point;
+}
+function back() {
+    let del = screen.value;
+    screen.value = del.slice(0, -1);
+}
